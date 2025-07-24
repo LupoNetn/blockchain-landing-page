ffd59c8fa92c009f653bc3b5f0f98f0b9be14d5e
oluwatobi <daniellupo30@gmail.com>
2025-07-24 02:43:20 +0100

    add Partners component and partner images for display

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
+import Partners from './components/Partners'
 
 const App = () => {
   return (
     <div className='bg-black'>
      <Navbar />
      <Hero />
+     <Partners />
     </div>
   )
 }
